964ca25397c4f7ecc38631fff59da5cddfd9071d
Matthew Moyer <matthewrmoyer@gmail.com>
2018-01-24 14:23:59 -0500

    mutations and types

--- server.js
+++ server.js
@@ -3,15 +3,44 @@ const graphqlHTTP = require('express-graphql')
 const { buildSchema } = require('graphql')
 
 const schema = buildSchema(`
+
+    input MessageInput {
+        author: String
+        content: String
+    }
+
+    type Message {
+        id: String
+        content: String
+        author: String
+    }
+
     type RandomDie {
         rollOnce : Int!
         roll(numRolls: Int) : [Int]
     }
 
     type Query {
+        getMessage(id: ID) : Message
         getDie(numSides: Int) : RandomDie
     }
+
+    type Mutation {
+        createMessage(input: MessageInput) : Message
+        updateMessage(id: Int!, input: MessageInput) : Message
+    }
 `)
+
+let fakeDatabase = {}
+
+class Message {
+    constructor(id, { author, content }) {
+        this.id = id
+        this.author = author,
+        this.content = content
+    }
+}
+
 
 class RandomDie {
     rollOnce () {
@@ -29,6 +58,26 @@ class RandomDie {
 const root = {
     getDie({ numSides }) {
         return new RandomDie(numSides)
+    },
+    getMessage({ id }) {
+        console.log(id);
+        
+        if(!fakeDatabase[id]) {
+            throw new Error(`no message with id of ${id} found`)
+        }
+        return new Message(id, fakeDatabase[id]);
+    },
+    createMessage({ input }) {
+        const id = require('crypto').randomBytes(10).toString('hex')
+        fakeDatabase[id] = input
+        return new Message(id, fakeDatabase[id].author, fakeDatabase[id].content)
+    },
+    updateMessage({ id, input}) {
+        if(!fakeDatabase[id]) {
+            throw new Error(`no message with id of ${id} found`)
+        }
+        fakeDatabase[id] = input
+        return new Message(id, fakeDatabase[id])
     }
 }
 
